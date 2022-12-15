1fa55a002a7490292fd5b591a64d38da27e87d61
Julardzija <120105960+Julardzija@users.noreply.github.com>
2022-12-15 13:51:08 +0100

    Update danish_league.py

--- danish_league.py
+++ danish_league.py
@@ -525,7 +525,7 @@ with row5_1:
     st.markdown('Investigate a variety of stats for each team. Which team scores the most goals per game?')    
     plot_x_per_team_selected = st.selectbox ("Which attribute do you want to analyze?", list(label_attr_dict_teams.keys()), key = 'attribute_team')
     plot_x_per_team_type = st.selectbox ("Which measure do you want to analyze?", types, key = 'measure_team')
-    specific_team_colors = st.checkbox("Use team specific color scheme")
+    specific_team_colors = st.checkbox("Use color scheme based on regions in Denmark")
 with row5_2:
     if all_teams_selected != 'Select teams manually (choose below)' or selected_teams:
         plot_x_per_team(plot_x_per_team_selected, plot_x_per_team_type)
