00847f95698c931e9c1dff1a1afc832f869f4ffa
Damian <100217112+datrz@users.noreply.github.com>
2022-12-17 13:28:09 +0100

    Update danish_league.py

--- danish_league.py
+++ danish_league.py
@@ -539,7 +539,7 @@ if all_teams_selected == 'Include all available teams':
         stadium_name = list(geo_sub.stadium_name)[0]
         coordinates = pd.DataFrame(np.column_stack([longitude, latitude]), columns=['lat', 'lon'])
 
-        st.markdown(f'🇩🇰 **Match {return_game_id_value_team[0]}** was played at **{stadium_name}, {city}, {reg}** and **{attendance1} fans** watched the game on the stadium')
+        st.markdown(f'🇩🇰 **Match {return_game_id_value_team[0]}** was played at **{stadium_name}, {city[0]}, {reg[0]}** and **{attendance1} fans** watched the game on the stadium')
         
         zoom_level = st.selectbox(
             "Zoom Level",
