4aa099b839344971588135c616da078fcb0a4597
Damian <100217112+datrz@users.noreply.github.com>
2022-12-17 12:52:28 +0100

    Update danish_league.py

--- danish_league.py
+++ danish_league.py
@@ -524,7 +524,7 @@ if all_teams_selected == 'Include all available teams':
     row18_spacer1, row18_1, row18_spacer2  = st.columns((0.5, 6, 0.5))
     with row18_1:
 
-        geo_sub = df_database[df_database.index == return_game_id_value_team[0]]
+        geo_sub = df_database[df_database.index == return_game_id_value_team[0]].index=False
 
         attendance = geo_sub.attendance
         st.subheader(attendance)
