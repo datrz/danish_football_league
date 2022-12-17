46bc8c0f9345a2413d9dad22bcae217c5617cd1e
Damian <100217112+datrz@users.noreply.github.com>
2022-12-17 21:11:26 +0100

    Update danish_league.py

--- danish_league.py
+++ danish_league.py
@@ -15,7 +15,7 @@ st.set_page_config(layout="wide")
 
 ### Data Import ###
 df_database = pd.read_csv("./data/data8.csv")
-#geo = pd.read_csv("./data/geo.csv")
+#geo = pd.read_csv("./data/geo.csv") 
 types = ["Mean","Total","Median","Maximum","Minimum"]
 label_attr_dict = {"Goals":"goals", "Points":"points","Halftime Goals":"ht_goals","Shots on target":"shots_on", "Shots off target":"shots_off","Ball Possession":"possession", "Fouls Committed":"fouls", "Yellow Cards":"yellow", "Red Cards":"red", "Corners":"corners","Pre Match Expected Goals":"pre_xg", "Post Match Expected Goals":"xg", "Winning odds":"odds"}
 label_attr_dict_teams = {"Goals Scored":"goals","Goals Received":"goals_received","Points received":"points","Halftime Goals Scored":"ht_goals","Halftime Goals Received":"halftime_goals_received", "Ball Possession":"possession", "Fouls Committed":"fouls", "Red Cards":"red", "Yellow Cards":"yellow", "Corners":"corners", "Pre Match Expected Goals":"pre_xg", "Post Match Expected Goals":"xg", "Winning odds":"odds"}
