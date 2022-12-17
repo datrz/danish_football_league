29775e8f8f933ac6058de7831777be7197fd2dc6
Damian <100217112+datrz@users.noreply.github.com>
2022-12-17 22:05:02 +0100

    Update danish_league.py

--- danish_league.py
+++ danish_league.py
@@ -53,13 +53,6 @@ def filter_season(df_data):
     df_filtered_season = df_data[df_data['season'].isin(seasons_selected)]
     return df_filtered_season
 
-def filter_region(df_data):
-    df_filtered_region = pd.DataFrame()
-    if all_regions_selected == 'Select regions manually (choose below)':
-        df_filtered_region = df_data[df_data['reg'].isin(selected_regions)]
-        return df_filtered_region
-    return df_data
-
 def filter_matchday(df_data):
     df_filtered_matchday = pd.DataFrame()
     matchdays_list = list(range(selected_matchdays[0], selected_matchdays[1]+1))
@@ -73,7 +66,14 @@ def filter_teams(df_data):
         return df_filtered_team
     return df_data
 
-def stack_home_away_dataframe(df_data):
+def filter_region(df_data):
+    df_filtered_region = pd.DataFrame()
+    if all_regions_selected == 'Select regions manually (choose below)':
+        df_filtered_region = df_data[df_data['reg'].isin(selected_regions)]
+        return df_filtered_region
+    return df_data
+
+stack_home_away_dataframe(df_data):
     df_data["game_id"] = df_data.index + 1
     delta_names = ['goals','ht_goals','shots_on','shots_off','possession','fouls','yellow','red','corners','points','pre_xg','xg','odds']
     for column in delta_names:
@@ -85,9 +85,9 @@ def stack_home_away_dataframe(df_data):
         df_data[a_delta_column] = df_data[a_column]-df_data[h_column]
     #st.dataframe(data=df_data)
     column_names = ['possession','yellow','red','corners','points','pre_xg','xg','odds','delta_goals','delta_ht_goals','delta_shots_on','delta_shots_off','delta_possession','delta_fouls','delta_yellow','delta_red','delta_corners','delta_points','delta_pre_xg','delta_xg','delta_odds']
-    h_column_names = ['game_id','season','matchday','h_team','h_goals','a_goals','h_ht_goals','a_ht_goals','h_shots_on','a_shots_on','h_shots_off','a_shots_off','h_fouls','a_fouls']
-    a_column_names = ['game_id','season','matchday','a_team','a_goals','h_goals','a_ht_goals','h_ht_goals','a_shots_on','h_shots_on','a_shots_off','h_shots_off','a_fouls','h_fouls']
-    column_names_new = ['game_id','season','matchday','location','team','goals','goals_received','ht_goals','ht_goals_received','shots_on','shots_on_test','shots_off','shots_off_test','fouls','got_fouled','possession','yellow','red','corners', 'points', 'pre_xg','xg', 'odds', 'delta_goals','delta_ht_goals','delta_shots_on','delta_shots_off','delta_possession','delta_fouls','delta_yellow','delta_red','delta_corners','delta_points','delta_pre_xg','delta_xg','delta_odds']
+    h_column_names = ['game_id','season','matchday','reg','h_team','h_goals','a_goals','h_ht_goals','a_ht_goals','h_shots_on','a_shots_on','h_shots_off','a_shots_off','h_fouls','a_fouls']
+    a_column_names = ['game_id','season','matchday','reg','a_team','a_goals','h_goals','a_ht_goals','h_ht_goals','a_shots_on','h_shots_on','a_shots_off','h_shots_off','a_fouls','h_fouls']
+    column_names_new = ['game_id','season','matchday','reg','location','team','goals','goals_received','ht_goals','ht_goals_received','shots_on','shots_on_test','shots_off','shots_off_test','fouls','got_fouled','possession','yellow','red','corners', 'points', 'pre_xg','xg', 'odds', 'delta_goals','delta_ht_goals','delta_shots_on','delta_shots_off','delta_possession','delta_fouls','delta_yellow','delta_red','delta_corners','delta_points','delta_pre_xg','delta_xg','delta_odds']
     for column in column_names: 
         h_column_names.append("h_" + column)
         a_column_names.append("a_" + column)
@@ -98,7 +98,7 @@ def stack_home_away_dataframe(df_data):
     df_home.columns = column_names_new
     df_away.columns = column_names_new
     df_total = df_home.append(df_away, ignore_index=True).sort_values(['game_id','season', 'matchday'], ascending=[True,True, True])
-    df_total_sorted = df_total[['game_id','season','matchday','location','team','goals','goals_received','delta_goals','ht_goals','ht_goals_received','delta_ht_goals','shots_on','shots_on_test','delta_shots_on','shots_off','shots_off_test','delta_shots_off','possession','delta_possession','fouls','got_fouled','delta_fouls','yellow','delta_yellow','red','delta_red','corners','delta_corners','points','delta_points','pre_xg','delta_pre_xg','xg','delta_xg','odds','delta_odds']]
+    df_total_sorted = df_total[['game_id','season','matchday','reg','location','team','goals','goals_received','delta_goals','ht_goals','ht_goals_received','delta_ht_goals','shots_on','shots_on_test','delta_shots_on','shots_off','shots_off_test','delta_shots_off','possession','delta_possession','fouls','got_fouled','delta_fouls','yellow','delta_yellow','red','delta_red','corners','delta_corners','points','delta_points','pre_xg','delta_pre_xg','xg','delta_xg','odds','delta_odds']]
     return df_total_sorted
 
 def group_measure_by_attribute(aspect,attribute,measure):
